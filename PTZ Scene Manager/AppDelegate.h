2288282dec35d454820417504ae5f9340ce6aee0
Lee Ann Rucker <lrucker@mac.com>
2023-01-24 14:55:32 -0800

    Initial Commit

---
+++ PTZ Scene Manager/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PTZ Scene Manager
+//
+//  Created by Lee Ann Rucker on 1/24/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ PTZ Scene Manager/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  PTZ Scene Manager
+//
+//  Created by Lee Ann Rucker on 1/24/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
